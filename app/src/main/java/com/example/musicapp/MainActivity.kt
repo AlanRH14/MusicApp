8d8e834186c79935122aaa44d25bb8bffd82746d
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-05-23 09:48:31 -0600

    Splash config animation part. 2

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -11,18 +11,36 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.musicapp.ui.theme.MusicAppTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private var isSplashScreenVisible = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition { isSplashScreenVisible }
+        }
         enableEdgeToEdge()
         setContent {
             MusicAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    Text(
+                        modifier = Modifier.padding(innerPadding),
+                        text = "Hello word!"
+                    )
                 }
             }
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(2000)
+            isSplashScreenVisible = false
         }
     }
 }
