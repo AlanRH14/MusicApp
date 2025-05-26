e28db490b784eb9da27f8eefdc7ecb5373ba55de
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-05-26 12:44:01 -0600

    Config ApiService injection

--- app/src/main/java/com/example/musicapp/MusicApp.kt
+++ app/src/main/java/com/example/musicapp/MusicApp.kt
@@ -1,8 +1,10 @@
 package com.example.musicapp
 
 import android.app.Application
+import com.example.musicapp.di.NetworkModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import org.koin.ksp.generated.module
 
 
 class MusicApp : Application() {
@@ -11,7 +13,7 @@ class MusicApp : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@MusicApp)
-            modules()
+            modules(NetworkModule().module)
         }
     }
 }
