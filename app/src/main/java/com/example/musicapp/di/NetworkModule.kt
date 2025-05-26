3b93678fb999478c27d071bac45f84208dad77e2
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-05-26 12:22:05 -0600

    Config network module

--- app/src/main/java/com/example/musicapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/musicapp/di/NetworkModule.kt
@@ -1,13 +1,21 @@
 package com.example.musicapp.di
 
+import com.example.musicapp.utils.Constants.BASE_URL
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.annotation.Module
 import org.koin.core.annotation.Single
 import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
 
 @Module
 class NetworkModule {
+    private val json = Json {
+        coerceInputValues = true
+        ignoreUnknownKeys = true
+    }
 
     @Single
     fun provideLoggingInterceptor(): HttpLoggingInterceptor =
@@ -22,10 +30,12 @@ class NetworkModule {
             .build()
 
     @Single
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
-        Retrofit.Builder()
-            .baseUrl("")
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        val contentType = "application/json".toMediaType()
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
             .client(okHttpClient)
-            .addConverterFactory()
+            .addConverterFactory(json.asConverterFactory(contentType = contentType))
             .build()
+    }
 }
