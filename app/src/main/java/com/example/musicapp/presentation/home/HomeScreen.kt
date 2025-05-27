c0432229e50dc0a373347b0826f99e4e7de6ae2c
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-05-27 08:57:42 -0600

    Config Home Module Home Screen & Loading Screen

--- app/src/main/java/com/example/musicapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/musicapp/presentation/home/HomeScreen.kt
@@ -1,9 +1,12 @@
 package com.example.musicapp.presentation.home
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
+import kotlinx.coroutines.flow.collectLatest
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -11,6 +14,15 @@ fun HomeScreen(
     navController: NavHostController,
     viewModel: HomeViewModel = koinViewModel()
 ) {
+    LaunchedEffect(true) {
+        viewModel.event.collectLatest {
+            when (it) {
+                is HomeEvent.ShowErrorMessage -> {
+                    Toast.makeText(navController.context, it.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 
     val state by viewModel.state.collectAsState()
 
---
+++ app/src/main/java/com/example/musicapp/presentation/widgets/LoadingScreen.kt
@@ -0,0 +1,28 @@
+package com.example.musicapp.presentation.widgets
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.musicapp.R
+
+@Composable
+fun LoadingScreen(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        CircularProgressIndicator()
+        Text(text = stringResource(R.string.loading))
+    }
+}
