bc936f60f100cd381800adac8b94bd34a697f112
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-05-27 12:45:23 -0600

    Config Onboarding Module Onboarding Event, Onboarding Screen

--- app/src/main/java/com/example/musicapp/presentation/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/example/musicapp/presentation/onboarding/OnboardingScreen.kt
@@ -1,7 +1,16 @@
 package com.example.musicapp.presentation.onboarding
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
+import com.example.musicapp.R
+import com.example.musicapp.presentation.widgets.ErrorScreen
+import com.example.musicapp.presentation.widgets.LoadingScreen
+import kotlinx.coroutines.flow.collectLatest
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -10,4 +19,27 @@ fun OnboardingScreen(
     viewModel: OnboardingViewModel = koinViewModel()
 ) {
 
+    LaunchedEffect(true) {
+        viewModel.event.collectLatest {
+            when (it) {
+                is OnboardingEvent.ShowErrorMessage -> {
+                    Toast.makeText(navController.context, it.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    val state by viewModel.state.collectAsState()
+
+    if (state.isLoading) {
+        LoadingScreen()
+    }
+
+    if (!state.errorMessage.isNullOrEmpty()) {
+        ErrorScreen(
+            errorMessage = state.errorMessage ?: "Uknown",
+            primaryButton = stringResource(R.string.retry),
+            onPrimaryButtonClicked = {}
+        )
+    }
 }
