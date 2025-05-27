131bbcf0166a30d8bc24779bd755c65601e0c44d
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-05-27 10:20:31 -0600

    Config Home Module Home Screen & Error Screen

--- app/src/main/java/com/example/musicapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/musicapp/presentation/home/HomeScreen.kt
@@ -5,7 +5,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
+import com.example.musicapp.R
 import com.example.musicapp.presentation.widgets.ErrorScreen
 import com.example.musicapp.presentation.widgets.LoadingScreen
 import kotlinx.coroutines.flow.collectLatest
@@ -13,6 +16,7 @@ import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun HomeScreen(
+    modifier: Modifier = Modifier,
     navController: NavHostController,
     viewModel: HomeViewModel = koinViewModel()
 ) {
@@ -29,13 +33,14 @@ fun HomeScreen(
     val state by viewModel.state.collectAsState()
 
     if (state.isLoading) {
-        LoadingScreen()
+        LoadingScreen(modifier = modifier)
     }
 
     if (!state.error.isNullOrEmpty()) {
         ErrorScreen(
+            modifier = modifier,
             errorMessage = state.error ?: "",
-            primaryButton = "",
+            primaryButton = stringResource(R.string.retry),
             onPrimaryButtonClicked = {},
         )
     }
