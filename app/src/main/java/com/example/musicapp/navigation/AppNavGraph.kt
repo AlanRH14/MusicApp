edfde33b6d5b6a3f62a2acc41512c7eb970425a6
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-05-28 08:12:39 -0600

    Navigation config

---
+++ app/src/main/java/com/example/musicapp/navigation/AppNavGraph.kt
@@ -0,0 +1,11 @@
+package com.example.musicapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun AppNavGraph(
+    navController: NavHostController
+) {
+
+}
