f50e21c38652d2d9191f2dd2bbd4b183bc530bfb
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-05-28 13:57:17 -0600

    Config Login Content part 2.

--- app/src/main/java/com/example/musicapp/presentation/login/widget/LoginScreenContent.kt
+++ app/src/main/java/com/example/musicapp/presentation/login/widget/LoginScreenContent.kt
@@ -8,10 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.musicapp.R
@@ -40,11 +44,22 @@ fun LoginScreenContent(
                 .fillMaxWidth()
         ) {
             Image(
-                modifier = Modifier.size(120.dp),
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(Alignment.Center),
                 painter = painterResource(R.drawable.ic_logo),
                 contentDescription = stringResource(R.string.image_logo),
             )
         }
+
+        Spacer(modifier = Modifier.size(16.dp))
+
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(R.string.login_text),
+            style = MaterialTheme.typography.titleLarge,
+            textAlign = TextAlign.Center,
+        )
     }
 }
 
