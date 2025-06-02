64dbf9759f784145cec0317e40ea3a77c76de1c4
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-02 13:36:27 -0600

    Login ViewModel config for event clicks

--- app/src/main/java/com/example/musicapp/presentation/login/LoginState.kt
+++ app/src/main/java/com/example/musicapp/presentation/login/LoginState.kt
@@ -2,5 +2,7 @@ package com.example.musicapp.presentation.login
 
 data class LoginState(
     val isLoading: Boolean = false,
-    val errorMessage: String? = null
+    val errorMessage: String? = null,
+    val email: String? = null,
+    val password: String? = null
 )
--- app/src/main/java/com/example/musicapp/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/example/musicapp/presentation/login/LoginViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.musicapp.presentation.login
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import org.koin.android.annotation.KoinViewModel
 
 @KoinViewModel
@@ -16,7 +19,21 @@ class LoginViewModel : ViewModel() {
     private val _event = MutableSharedFlow<LoginEvent>()
     val event = _event.asSharedFlow()
 
-    fun fetchData() {
+    fun fetchData() {}
 
+    fun onLoginClicked() {
+
+    }
+
+    fun onRegisterClicked() {
+        viewModelScope.launch {
+            _event.emit(LoginEvent.ShowErrorMessage("Register clicked"))
+        }
+    }
+
+    fun onForgotPasswordClicked() {
+        viewModelScope.launch {
+            _event.emit(LoginEvent.ShowErrorMessage("Forgot Password clicked"))
+        }
     }
 }
