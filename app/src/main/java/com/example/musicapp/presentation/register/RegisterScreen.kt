949c46555d9d2bee5cbd080df83a85451559d66b
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-05 12:51:16 -0600

    Config Register Module part. 3.

--- app/src/main/java/com/example/musicapp/presentation/register/RegisterScreen.kt
+++ app/src/main/java/com/example/musicapp/presentation/register/RegisterScreen.kt
@@ -4,8 +4,12 @@ import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import com.example.musicapp.R
+import com.example.musicapp.presentation.widgets.ErrorScreen
+import com.example.musicapp.presentation.widgets.LoadingScreen
 import kotlinx.coroutines.flow.collectLatest
 import org.koin.androidx.compose.koinViewModel
 
@@ -24,6 +28,17 @@ fun RegisterScreen(
                 }
             }
         }
+    }
 
+    if (state.loading) {
+        LoadingScreen()
+    }
+
+    if (!state.errorMessage.isNullOrEmpty()) {
+        ErrorScreen(
+            errorMessage = state.errorMessage ?: stringResource(R.string.unknown),
+            primaryButton = stringResource(R.string.retry),
+            onPrimaryButtonClicked = {}
+        )
     }
 }
--- app/src/main/java/com/example/musicapp/presentation/register/RegisterState.kt
+++ app/src/main/java/com/example/musicapp/presentation/register/RegisterState.kt
@@ -1,5 +1,6 @@
 package com.example.musicapp.presentation.register
 
 data class RegisterState(
-    val loading: Boolean = false
+    val loading: Boolean = false,
+    val errorMessage: String? = null,
 )
