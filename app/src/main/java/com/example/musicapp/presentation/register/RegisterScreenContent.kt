fea362de8e5d434aa61e8b2fb97055d365f6250f
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-06 13:51:10 -0600

    Config RegisterScreenContent

---
+++ app/src/main/java/com/example/musicapp/presentation/register/RegisterScreenContent.kt
@@ -0,0 +1,19 @@
+package com.example.musicapp.presentation.register
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.musicapp.ui.theme.PaddingLarge
+
+@Composable
+fun RegisterScreenContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(PaddingLarge)
+    ) {
+
+    }
+}
