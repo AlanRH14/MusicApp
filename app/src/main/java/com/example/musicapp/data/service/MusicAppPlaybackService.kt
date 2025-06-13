6323e5afc59b540f6d4143a17f8f65dea684a05e
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-13 13:16:05 -0600

    Config Music App Notification helper part 6.

--- app/src/main/java/com/example/musicapp/data/service/MusicAppPlaybackService.kt
+++ app/src/main/java/com/example/musicapp/data/service/MusicAppPlaybackService.kt
@@ -6,6 +6,15 @@ import android.os.IBinder
 
 class MusicAppPlaybackService: Service() {
 
+    companion object {
+        private const val ACTION_PLAY = "com.example.musicapp.ACTION_PLAY"
+        private const val ACTION_PAUSE = "com.example.musicapp.ACTION_PAUSE"
+        private const val ACTION_STOP = "com.example.musicapp.ACTION_STOP"
+        private const val ACTION_PREVIOUS = "com.example.musicapp.ACTION_PREVIOUS"
+        private const val ACTION_NEXT = "com.example.musicapp.ACTION_NEXT"
+        private const val ACTION_PREPARE_SONG = "com.example.musicapp.ACTION_PREPAPRE_SONG"
+    }
+
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
