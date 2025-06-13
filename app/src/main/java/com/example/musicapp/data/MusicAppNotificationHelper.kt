fe1b61ecda829b418b124c995db13e70e9d27fdd
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-13 11:24:44 -0600

    Config Music App Notification helper.

---
+++ app/src/main/java/com/example/musicapp/data/MusicAppNotificationHelper.kt
@@ -0,0 +1,36 @@
+package com.example.musicapp.data
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+
+class MusicAppNotificationHelper(private val mContext: Context) {
+
+    companion object {
+        const val CHANNEL_ID = "musicapp_notification_channel"
+        const val CHANNEL_NAME = "Notification Channel"
+        const val CHANNEL_DESCRIPTION = "Channel for MusicApp playback notifications"
+        const val NOTIFICATION_ID = 1
+
+        fun createNotificationChannel(mContext: Context) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(
+                    CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT,
+                ).apply {
+                    description = CHANNEL_DESCRIPTION
+                    setSound(null, null)
+                    setShowBadge(false)
+                    lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+                }
+
+                val notificationManager =
+                    mContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(channel)
+            }
+        }
+    }
+}
