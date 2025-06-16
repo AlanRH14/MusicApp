bb0cea7136529268d4d053c477f52d2c60487c73
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-16 10:25:22 -0600

    Config Home Screen part. 1.

---
+++ app/src/main/java/com/example/musicapp/presentation/home/component/HomeHeader.kt
@@ -0,0 +1,8 @@
+package com.example.musicapp.presentation.home.component
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeHeader() {
+
+}
---
+++ app/src/main/java/com/example/musicapp/presentation/home/widget/HomeContent.kt
@@ -0,0 +1,8 @@
+package com.example.musicapp.presentation.home.widget
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeContent() {
+
+}
