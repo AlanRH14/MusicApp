989eb2ba00bf2d317e7cdb26738598a4739f3043
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-16 12:16:03 -0600

    Config Home Screen part. 4.

--- app/src/main/java/com/example/musicapp/presentation/home/widget/HomeContent.kt
+++ app/src/main/java/com/example/musicapp/presentation/home/widget/HomeContent.kt
@@ -1,7 +1,13 @@
 package com.example.musicapp.presentation.home.widget
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.musicapp.R
 import com.example.musicapp.presentation.home.HomeState
+import com.example.musicapp.presentation.home.component.HomeHeader
 
 @Composable
 fun HomeContent(
@@ -10,4 +16,13 @@ fun HomeContent(
     onAlbumClicked: () -> Unit,
 ) {
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        HomeHeader(
+            userName = state.user.name ?: "",
+            painterResource(R.drawable.ic_profile)
+        )
+    }
 }
