a33c58fc4574942b1b8740e929c5f796f0d5276c
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-16 13:25:30 -0600

    Add Models ui part. 5

--- app/src/main/java/com/example/musicapp/domain/model/Home.kt
+++ app/src/main/java/com/example/musicapp/domain/model/Home.kt
@@ -1,11 +1,7 @@
 package com.example.musicapp.domain.model
 
-import com.example.musicapp.data.model.AlbumDto
-import com.example.musicapp.data.model.ContinueListeningDto
-import com.example.musicapp.data.model.SongDto
-
 data class Home(
-    val continueListening: List<ContinueListeningDto> = emptyList(),
-    val recommendedSong: List<SongDto> = emptyList(),
-    val topMixes: List<AlbumDto> = emptyList(),
+    val continueListening: List<ContinueListening> = emptyList(),
+    val recommendedSong: List<Song> = emptyList(),
+    val topMixes: List<Album> = emptyList(),
 )
