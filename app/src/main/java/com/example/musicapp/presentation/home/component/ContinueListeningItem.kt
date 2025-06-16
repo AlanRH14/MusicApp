5f22b61bb1c8957acd56c6d9d3bc1906d1ff5215
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-16 17:03:04 -0600

    Config Home Screen part. 11.

--- app/src/main/java/com/example/musicapp/presentation/home/component/ContinueListeningItem.kt
+++ app/src/main/java/com/example/musicapp/presentation/home/component/ContinueListeningItem.kt
@@ -1,8 +1,51 @@
 package com.example.musicapp.presentation.home.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.musicapp.R
+import com.example.musicapp.domain.model.Song
+import com.example.musicapp.presentation.common.components.MusicAppImage
+import com.example.musicapp.ui.theme.PaddingDefault
+import com.example.musicapp.ui.theme.PaddingSmall
+import com.example.musicapp.ui.theme.Shapes
 
 @Composable
-fun ContinueListeningItem() {
+fun ContinueListeningItem(song: Song) {
+    Row(
+        modifier = Modifier
+            .padding(PaddingSmall)
+            .fillMaxWidth()
+            .padding(PaddingDefault)
+            .clip(Shapes.small)
+            .background(Color.Gray)
+    ) {
+        MusicAppImage(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(Shapes.small),
+            pathImage = song.coverImage,
+            imageDefault = R.drawable.ic_logo,
+            placeHolder = {},
+            contentDescription = stringResource(R.string.continue_listening_image),
+        )
 
+        Text(
+            text = song.title,
+            maxLines = 1,
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+    }
 }
--- app/src/main/java/com/example/musicapp/presentation/home/widget/ContinueListeningSection.kt
+++ app/src/main/java/com/example/musicapp/presentation/home/widget/ContinueListeningSection.kt
@@ -1,8 +1,36 @@
 package com.example.musicapp.presentation.home.widget
 
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.musicapp.domain.model.Song
+import com.example.musicapp.presentation.home.component.ContinueListeningItem
+import com.example.musicapp.ui.theme.PaddingDefault
+import com.example.musicapp.utils.Constants.GRID_CELLS_SIZE
 
 @Composable
-fun ContinueListeningSection() {
+fun ColumnScope.ContinueListeningSection(
+    songs: List<Song>
+) {
+    Text(
+        text = "Continue Listening",
+        style = MaterialTheme.typography.titleMedium
+    )
 
+    Spacer(modifier = Modifier.size(PaddingDefault))
+
+    LazyVerticalGrid(columns = GridCells.Fixed(GRID_CELLS_SIZE)) {
+        items(songs, key = { it.id }) { song ->
+            ContinueListeningItem(
+                song = song
+            )
+        }
+    }
 }
