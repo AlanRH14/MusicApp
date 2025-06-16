5e99bdbf0df649e88cb8e688a08a0dbb49d041e0
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-16 12:37:36 -0600

    Add module Domain & Dto Models/ Models ui part. 7

--- app/src/main/java/com/example/musicapp/data/model/AlbumDto.kt
+++ app/src/main/java/com/example/musicapp/data/model/AlbumDto.kt
@@ -5,11 +5,11 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AlbumDto(
-    @SerialName("artist") val artistDto: ArtistDto,
+    @SerialName("artist") val artist: ArtistDto,
     @SerialName("coverImage") val coverImage: String,
     @SerialName("genre") val genre: String,
     @SerialName("id") val id: String,
     @SerialName("releaseDate") val releaseDate: Long,
-    @SerialName("songs") val songDtos: List<SongDto>,
+    @SerialName("songs") val song: List<SongDto>,
     @SerialName("title") val title: String
 )
--- app/src/main/java/com/example/musicapp/data/model/reponse/HomeResponse.kt
+++ app/src/main/java/com/example/musicapp/data/model/reponse/HomeResponse.kt
@@ -9,9 +9,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class HomeResponse(
     @SerialName("continueListening")
-    val continueListeningDto: List<ContinueListeningDto>? = null,
+    val continueListening: List<ContinueListeningDto>? = null,
     @SerialName("recommendedSongs")
-    val recommendedSongDtos: List<SongDto>? = null,
+    val recommendedSong: List<SongDto>? = null,
     @SerialName("topMixes")
     val topMixes: List<AlbumDto>? = null
 )
