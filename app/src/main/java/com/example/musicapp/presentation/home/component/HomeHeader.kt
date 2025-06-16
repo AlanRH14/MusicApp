4e8e2503f7f60851cc7f91b0f84c3bcfe221c854
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-16 12:04:39 -0600

    Config Home Screen part. 2.

--- app/src/main/java/com/example/musicapp/presentation/home/component/HomeHeader.kt
+++ app/src/main/java/com/example/musicapp/presentation/home/component/HomeHeader.kt
@@ -1,8 +1,60 @@
 package com.example.musicapp.presentation.home.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.musicapp.R
+import com.example.musicapp.ui.theme.PaddingLarge
+import com.example.musicapp.ui.theme.PaddingSmall
 
 @Composable
-fun HomeHeader() {
+fun HomeHeader(
+    userName: String,
+    userImage: Painter
+) {
 
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(PaddingLarge)
+    ) {
+        Image(
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(24.dp),
+            painter = userImage,
+            contentDescription = stringResource(R.string.user_image)
+        )
+
+        Spacer(modifier = Modifier.size(PaddingLarge))
+
+        Column {
+            Text(
+                text = "Welcome Back!",
+                color = MaterialTheme.colorScheme.onPrimary,
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Spacer(modifier = Modifier.size(PaddingSmall))
+
+            Text(
+                text = userName,
+                color = MaterialTheme.colorScheme.onPrimary,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
 }
--- app/src/main/java/com/example/musicapp/presentation/home/widget/HomeContent.kt
+++ app/src/main/java/com/example/musicapp/presentation/home/widget/HomeContent.kt
@@ -1,8 +1,13 @@
 package com.example.musicapp.presentation.home.widget
 
 import androidx.compose.runtime.Composable
+import com.example.musicapp.presentation.home.HomeState
 
 @Composable
-fun HomeContent() {
+fun HomeContent(
+    state: HomeState,
+    onSongClicked: () -> Unit,
+    onAlbumClicked: () -> Unit,
+) {
 
 }
