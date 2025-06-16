943279733e112ada04422050de72f37e002e5048
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-16 08:31:07 -0600

    Config Home Response part 5.

--- app/src/main/java/com/example/musicapp/data/model/Album.kt
+++ app/src/main/java/com/example/musicapp/data/model/Album.kt
@@ -1,11 +1,15 @@
 package com.example.musicapp.data.model
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Album(
-    val artist: Artist,
-    val coverImage: String,
-    val genre: String,
-    val id: String,
-    val releaseDate: Long,
-    val songs: List<Song>,
-    val title: String
+    @SerialName("artist") val artist: Artist,
+    @SerialName("coverImage") val coverImage: String,
+    @SerialName("genre") val genre: String,
+    @SerialName("id") val id: String,
+    @SerialName("releaseDate") val releaseDate: Long,
+    @SerialName("songs") val songs: List<Song>,
+    @SerialName("title") val title: String
 )
