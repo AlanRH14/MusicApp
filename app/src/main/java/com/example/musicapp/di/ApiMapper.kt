bf27492f1d9ae7cf1d3843a897f39981a493d42d
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-17 12:49:30 -0600

    Config Architecture part. 19.

--- app/src/main/java/com/example/musicapp/di/ApiMapper.kt
+++ app/src/main/java/com/example/musicapp/di/ApiMapper.kt
@@ -1,45 +1,16 @@
 package com.example.musicapp.di
 
-import com.example.musicapp.common.music.ApiMapper
 import com.example.musicapp.data.mapper_impl.artist.ArtistApiMapperImpl
 import com.example.musicapp.data.mapper_impl.home.HomeApiMapperImpl
 import com.example.musicapp.data.mapper_impl.login.LoginApiMapperImpl
 import com.example.musicapp.data.mapper_impl.song.SongApiMapperImpl
 import com.example.musicapp.data.mapper_impl.user.UserApiMapperImpl
-import com.example.musicapp.data.model.ArtistDto
-import com.example.musicapp.data.model.SongDto
-import com.example.musicapp.data.model.UserDto
-import com.example.musicapp.data.model.reponse.HomeResponse
-import com.example.musicapp.data.model.reponse.LoginResponse
-import com.example.musicapp.domain.model.Artist
-import com.example.musicapp.domain.model.Home
-import com.example.musicapp.domain.model.Login
-import com.example.musicapp.domain.model.Song
-import com.example.musicapp.domain.model.User
-import org.koin.core.annotation.Module
-import org.koin.core.annotation.Single
+import org.koin.dsl.module
 
-@Module
-class ApiMapperModule {
-
-    @Single
-    fun provideUserApiMapper(): ApiMapper<UserDto, User> =
-        UserApiMapperImpl()
-
-    @Single
-    fun provideLoginApiMapper(apiUserMapper: UserApiMapperImpl): ApiMapper<LoginResponse, Login> =
-        LoginApiMapperImpl(apiUserMapper = apiUserMapper)
-
-
-    @Single
-    fun provideArtistMapper(): ApiMapper<ArtistDto, Artist> =
-        ArtistApiMapperImpl()
-
-    @Single
-    fun provideSongApiMapper(apiArtistMapper: ArtistApiMapperImpl): ApiMapper<SongDto, Song> =
-        SongApiMapperImpl(apiArtistMapper = apiArtistMapper)
-
-    @Single
-    fun provideHomeApiMapper(apiSongMapper: SongApiMapperImpl): ApiMapper<HomeResponse, Home> =
-        HomeApiMapperImpl(apiSongMapper)
+val apiMapperModule = module {
+    single { UserApiMapperImpl() }
+    single { LoginApiMapperImpl(apiUserMapper = get()) }
+    single { ArtistApiMapperImpl() }
+    single { SongApiMapperImpl(apiArtistMapper = get()) }
+    single { HomeApiMapperImpl(apiSongMapper = get()) }
 }
