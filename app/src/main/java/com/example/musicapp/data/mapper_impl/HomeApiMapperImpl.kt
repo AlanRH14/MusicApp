b95a6fff5049592f7f716ca197d03a8b2de51f1f
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-17 11:34:53 -0600

    Config Architecture part. 14.

--- app/src/main/java/com/example/musicapp/data/mapper_impl/HomeApiMapperImpl.kt
+++ app/src/main/java/com/example/musicapp/data/mapper_impl/HomeApiMapperImpl.kt
@@ -4,9 +4,13 @@ import com.example.musicapp.common.music.ApiMapper
 import com.example.musicapp.data.model.reponse.HomeResponse
 import com.example.musicapp.domain.model.Home
 
-class HomeApiMapperImpl: ApiMapper<HomeResponse, Home> {
+class HomeApiMapperImpl : ApiMapper<HomeResponse, Home> {
 
     override fun mapToDomain(apiDto: HomeResponse): Home {
-        TODO("Not yet implemented")
+        return Home(
+            continueListening = apiDto.continueListening ?: emptyList(),
+            recommendedSong = emptyList(),
+            topMixes = emptyList(),
+        )
     }
 }
