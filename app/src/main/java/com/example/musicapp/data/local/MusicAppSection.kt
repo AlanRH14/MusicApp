f5061d91b8cb93ad0fd23ace66af1e6504e7a341
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-18 09:01:07 -0600

    Init Persistence App with DataStore part. 1.

--- app/src/main/java/com/example/musicapp/data/local/MusicAppSection.kt
+++ app/src/main/java/com/example/musicapp/data/local/MusicAppSection.kt
@@ -1,7 +1,19 @@
 package com.example.musicapp.data.local
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.musicapp.utils.Constants.ONBOARDING_PREFERENCES_KEY
+import com.example.musicapp.utils.Constants.PREFERENCES_NAME
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = PREFERENCES_NAME)
 
 class MusicAppSection(private val mContext: Context) {
+
+    private object PreferencesKey {
+        val onBoardingKey = booleanPreferencesKey(name = ONBOARDING_PREFERENCES_KEY)
+    }
 
 }
--- app/src/main/java/com/example/musicapp/utils/Constants.kt
+++ app/src/main/java/com/example/musicapp/utils/Constants.kt
@@ -3,4 +3,7 @@ package com.example.musicapp.utils
 object Constants {
     const val BASE_URL = "http://10.0.2.2:8080/"
     const val GRID_CELLS_SIZE = 2
+
+    const val PREFERENCES_NAME = "musicapp_preferences"
+    const val ONBOARDING_PREFERENCES_KEY = "onboarding_completed"
 }
