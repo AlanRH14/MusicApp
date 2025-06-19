276c49f26ca87f1bd4ebb972bafd49c38ad2def4
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-19 11:57:11 -0600

    Refactor Modules part. 1.

---
+++ app/src/main/java/com/example/musicapp/utils/PreferencesConntants.kt
@@ -0,0 +1,8 @@
+package com.example.musicapp.utils
+
+object PreferencesConstants {
+
+    const val PREFERENCES_NAME = "musicapp_preferences"
+    const val TOKEN_PREFERENCES_KEY = "token"
+    const val USER_NAME_PREFERENCES_KEY = "user_name"
+}
