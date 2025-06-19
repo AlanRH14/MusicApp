42f74db8e62a8182153881524dd6b744c4d4d420
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-19 08:00:34 -0600

    Init Persistence App with DataStore part. 30.

--- app/src/main/java/com/example/musicapp/di/DataStoreModule.kt
+++ app/src/main/java/com/example/musicapp/di/DataStoreModule.kt
@@ -1,7 +1,24 @@
 package com.example.musicapp.di
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import org.koin.dsl.module
 
 val dataStoreModule = module {
-
+    single<DataStore<Preferences>> {
+        PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler(
+                produceNewData = { emptyPreferences() }
+            ),
+            migrations = listOf(),
+            scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+            produceFile = { context -> context.preferencesDataStoreFile("") }
+        )
+    }
 }
