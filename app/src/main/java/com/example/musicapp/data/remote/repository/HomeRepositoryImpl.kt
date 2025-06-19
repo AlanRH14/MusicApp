312dff767f91a0ff0d0bd0631461d35db11df2e2
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-19 16:14:05 -0600

    Get Home Data Error

--- app/src/main/java/com/example/musicapp/data/remote/repository/HomeRepositoryImpl.kt
+++ app/src/main/java/com/example/musicapp/data/remote/repository/HomeRepositoryImpl.kt
@@ -21,8 +21,9 @@ class HomeRepositoryImpl(
                 response.body()?.let { res ->
                     Resource.Success(data = apiHomeMapper.mapToDomain(apiDto = res))
                 } ?: Resource.Success(data = Home())
+            }else {
+                Resource.Success(data = Home())
             }
-            Resource.Success(data = Home())
         } catch (e: Exception) {
             Resource.Error("")
         }
