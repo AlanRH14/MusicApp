84622604b2bb0f99a03a784a069ca57c2dba2d23
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-19 08:16:40 -0600

    Init Persistence App with DataStore part. 34.

--- app/src/main/java/com/example/musicapp/presentation/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/example/musicapp/presentation/onboarding/OnboardingViewModel.kt
@@ -2,22 +2,33 @@ package com.example.musicapp.presentation.onboarding
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.musicapp.common.ConstantsPreferences
+import com.example.musicapp.domain.repository.DataStoreHandle
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class OnboardingViewModel : ViewModel() {
+class OnboardingViewModel(
+    private val dataStoreHandle: DataStoreHandle
+) : ViewModel() {
     private val _state = MutableStateFlow(OnboardingState())
     val state = _state.asStateFlow()
 
     private val _event = MutableSharedFlow<OnboardingEvent>()
     val event = _event.asSharedFlow()
 
+    fun isUserLoggedIn() {
+        viewModelScope.launch {
+            dataStoreHandle.readState(key = ConstantsPreferences.TokenPreferences).collect {
+
+            }
+        }
+    }
+
     fun onGetStartedClicked() {
         viewModelScope.launch {
-            //TODO: Update the storage to no show the onboarding again
             _event.emit(OnboardingEvent.NavigationToLogin)
         }
     }
