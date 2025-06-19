0089bf53bcb812db9de752e00f3bcd587bc615e6
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-19 14:57:03 -0600

    Refactor Modules part. 9.

--- app/src/main/java/com/example/musicapp/data/local/ConstantsPreferences.kt
+++ app/src/main/java/com/example/musicapp/data/local/ConstantsPreferences.kt
@@ -1,4 +1,4 @@
-package com.example.musicapp.domain
+package com.example.musicapp.data.local
 
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.stringPreferencesKey
