d4de80be906ba786e239bd4c1887b602c2ec35c6
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-20 12:27:14 -0600

    Config room Logic part. 8.

--- app/src/main/java/com/example/musicapp/data/local/database/entities/UserEntity.kt
+++ app/src/main/java/com/example/musicapp/data/local/database/entities/UserEntity.kt
@@ -2,12 +2,13 @@ package com.example.musicapp.data.local.database.entities
 
 import androidx.room.Entity
 import com.example.musicapp.utils.DatabaseConstants.USER_TABLE
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 @Entity(tableName = USER_TABLE)
 data class UserEntity(
-    @serval token: String,
-    val name: String,
-    val profilePicture: String
+    @SerialName("token") val token: String,
+    @SerialName("name") val name: String,
+    @SerialName("profilePicture") val profilePicture: String
 )
