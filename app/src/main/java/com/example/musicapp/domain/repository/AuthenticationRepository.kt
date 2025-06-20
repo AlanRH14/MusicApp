6c43f3b8ba7ca7126ca012ebc38021dad8d209e9
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-20 13:49:38 -0600

    Config room Logic part. 18.

--- app/src/main/java/com/example/musicapp/domain/repository/AuthenticationRepository.kt
+++ app/src/main/java/com/example/musicapp/domain/repository/AuthenticationRepository.kt
@@ -4,10 +4,11 @@ import com.example.musicapp.data.model.request.LoginRequest
 import com.example.musicapp.data.model.request.RegisterRequest
 import com.example.musicapp.domain.model.Login
 import com.example.musicapp.common.Resource
+import com.example.musicapp.domain.model.User
 
 interface AuthenticationRepository {
 
-    suspend fun login(loginRequest: LoginRequest): Resource<Login>
+    suspend fun login(loginRequest: LoginRequest): Resource<User>
 
-    suspend fun register(registerRequest: RegisterRequest): Resource<Login>
+    suspend fun register(registerRequest: RegisterRequest): Resource<User>
 }
