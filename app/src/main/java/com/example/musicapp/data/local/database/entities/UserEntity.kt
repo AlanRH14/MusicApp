4dd77b174da7973c00656599b61e4420b12d0a78
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-20 11:52:28 -0600

    Config room Logic part. 6.

---
+++ app/src/main/java/com/example/musicapp/data/local/database/entities/UserEntity.kt
@@ -0,0 +1,9 @@
+package com.example.musicapp.data.local.database.entities
+
+import androidx.room.Entity
+import com.example.musicapp.utils.DatabaseConstants.USER_TABLE
+
+@Entity(tableName = USER_TABLE)
+data class UserEntity(
+
+)
---
+++ app/src/main/java/com/example/musicapp/utils/DatabaseConstants.kt
@@ -0,0 +1,7 @@
+package com.example.musicapp.utils
+
+object DatabaseConstants {
+
+    const val DATABASE = ""
+    const val USER_TABLE = "user_table"
+}
