d91b49f7f66ea5d0e8f4a9806025fa381c1592f1
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-20 11:33:30 -0600

    Config room Logic part. 4.

--- app/src/main/java/com/example/musicapp/data/local/database/MusicAppDao.kt
+++ app/src/main/java/com/example/musicapp/data/local/database/MusicAppDao.kt
@@ -1,4 +1,12 @@
 package com.example.musicapp.data.local.database
 
-interface MusicAppDao {
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+
+interface MusicAppDao<T> {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(entities: T)
+
+    suspend fun update(entities: T)
 }
