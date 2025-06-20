fa5b72f8e0a9b86ccf80ab9fbf95bbb8d08bf74e
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-06-20 12:40:36 -0600

    Config room Logic part. 10.

--- app/src/main/java/com/example/musicapp/data/mapper_impl/user/UserEntityMapperImpl.kt
+++ app/src/main/java/com/example/musicapp/data/mapper_impl/user/UserEntityMapperImpl.kt
@@ -4,9 +4,13 @@ import com.example.musicapp.common.ApiMapper
 import com.example.musicapp.data.local.database.entities.UserEntity
 import com.example.musicapp.data.model.UserDto
 
-class UserEntityMapperImpl: ApiMapper<UserDto, UserEntity> {
+class UserEntityMapperImpl : ApiMapper<UserDto, UserEntity> {
 
     override fun mapToDomain(apiDto: UserDto): UserEntity {
-        TODO("Not yet implemented")
+        return UserEntity(
+            token = apiDto.id ?: "",
+            name = apiDto.name ?: "",
+            profilePicture = apiDto.profilePicture ?: ""
+        )
     }
 }
