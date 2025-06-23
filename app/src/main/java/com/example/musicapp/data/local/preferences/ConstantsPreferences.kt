6cf218df2d690c38e9ea32055c497449b1fed0f9
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-23 12:26:04 -0600

    Change config User Is Logged logic part. 3.

--- app/src/main/java/com/example/musicapp/data/local/preferences/ConstantsPreferences.kt
+++ app/src/main/java/com/example/musicapp/data/local/preferences/ConstantsPreferences.kt
@@ -2,10 +2,8 @@ package com.example.musicapp.data.local.preferences
 
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.stringPreferencesKey
 import com.example.musicapp.common.PreferencesKey
 import com.example.musicapp.utils.PreferencesConstants.TOKEN_PREFERENCES_KEY
-import com.example.musicapp.utils.PreferencesConstants.USER_NAME_PREFERENCES_KEY
 
 internal sealed class ConstantsPreferences<T> : PreferencesKey<T> {
 
