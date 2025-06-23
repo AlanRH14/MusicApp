4f2f1197b6787f721d6daf288013b94cc60253a8
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-23 13:46:19 -0600

    Config TopMixesSection part. 14.

--- app/src/main/java/com/example/musicapp/presentation/home/widget/HomeContent.kt
+++ app/src/main/java/com/example/musicapp/presentation/home/widget/HomeContent.kt
@@ -1,8 +1,10 @@
 package com.example.musicapp.presentation.home.widget
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.musicapp.R
@@ -33,9 +35,13 @@ fun HomeContent(
             onItemClicked = {}
         )
 
+        Spacer(modifier = Modifier.size(PaddingLarge))
+
         TopMixesSection(
             albums = state.homData.topMixes,
             onAlbumClicked = {}
         )
+
+        Spacer(modifier = Modifier.size(PaddingLarge))
     }
 }
