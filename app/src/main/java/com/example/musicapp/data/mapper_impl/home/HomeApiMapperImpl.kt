988f02bd0375d00493bc8202bfb813383107399b
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-23 13:34:43 -0600

    Config TopMixesSection part. 9.

--- app/src/main/java/com/example/musicapp/data/mapper_impl/home/HomeApiMapperImpl.kt
+++ app/src/main/java/com/example/musicapp/data/mapper_impl/home/HomeApiMapperImpl.kt
@@ -1,13 +1,16 @@
 package com.example.musicapp.data.mapper_impl.home
 
 import com.example.musicapp.common.ApiMapper
+import com.example.musicapp.data.model.AlbumDto
 import com.example.musicapp.data.model.SongDto
 import com.example.musicapp.data.model.reponse.HomeResponse
+import com.example.musicapp.domain.model.Album
 import com.example.musicapp.domain.model.Home
 import com.example.musicapp.domain.model.Song
 
 class HomeApiMapperImpl(
-    private val apiSongMapper: ApiMapper<SongDto, Song>
+    private val apiSongMapper: ApiMapper<SongDto, Song>,
+    private val apiAlbumMapper: ApiMapper<AlbumDto, Album>
 ) : ApiMapper<HomeResponse, Home> {
 
     override fun mapToDomain(apiDto: HomeResponse): Home {
@@ -16,7 +19,9 @@ class HomeApiMapperImpl(
                 apiSongMapper.mapToDomain(continueListening)
             } ?: emptyList(),
             recommendedSong = emptyList(),
-            topMixes = emptyList(),
+            topMixes = apiDto.topMixes?.map { album ->
+                apiAlbumMapper.mapToDomain(album)
+            } ?: emptyList(),
         )
     }
 }
