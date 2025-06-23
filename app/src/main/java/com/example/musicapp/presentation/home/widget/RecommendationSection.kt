a09f6c54c6b9d9eabc4e52d5edceed126ea99b84
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-23 16:16:55 -0600

    Config TopMixesSection part. 28.

--- app/src/main/java/com/example/musicapp/presentation/home/widget/RecommendationSection.kt
+++ app/src/main/java/com/example/musicapp/presentation/home/widget/RecommendationSection.kt
@@ -1,5 +1,6 @@
 package com.example.musicapp.presentation.home.widget
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -9,16 +10,21 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.musicapp.R
 import com.example.musicapp.domain.model.Song
+import com.example.musicapp.presentation.common.components.MusicAppImage
 import com.example.musicapp.presentation.home.component.AlbumItem
 import com.example.musicapp.ui.theme.PaddingDefault
 import com.example.musicapp.ui.theme.PaddingSmall
+import com.example.musicapp.ui.theme.Shapes
 
 @Composable
 fun RecommendationSection(
     songs: List<Song>,
+    onRecommendationClicked: (String) -> Unit
 ) {
     Text(
         text = stringResource(R.string.recommendation),
@@ -31,7 +37,16 @@ fun RecommendationSection(
         modifier = Modifier.padding(PaddingSmall)
     ) {
         items(items = songs, key = { it.id }) { song ->
-
+            MusicAppImage(
+                modifier = Modifier
+                    .size(150.dp)
+                    .clickable { onRecommendationClicked(song.id) }
+                    .clip(Shapes.medium),
+                pathImage = song.coverImage,
+                imageDefault = R.drawable.ic_logo,
+                placeHolder = {},
+                contentDescription = "Song Recommendation Image",
+            )
         }
     }
 }
