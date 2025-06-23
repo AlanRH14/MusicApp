fe6998dbdf3ffa242c997d4820708e12c6211988
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-23 12:19:11 -0600

    Change config User Is Logged logic

--- app/src/main/java/com/example/musicapp/data/local/preferences/ConstantsPreferences.kt
+++ app/src/main/java/com/example/musicapp/data/local/preferences/ConstantsPreferences.kt
@@ -1,6 +1,7 @@
 package com.example.musicapp.data.local.preferences
 
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import com.example.musicapp.common.PreferencesKey
 import com.example.musicapp.utils.PreferencesConstants.TOKEN_PREFERENCES_KEY
@@ -8,10 +9,10 @@ import com.example.musicapp.utils.PreferencesConstants.USER_NAME_PREFERENCES_KEY
 
 internal sealed class ConstantsPreferences<T> : PreferencesKey<T> {
 
-    data object TokenPreferences : ConstantsPreferences<String>() {
+    data object UserIsLoggedPreferences : ConstantsPreferences<Boolean>() {
         override val key: String = TOKEN_PREFERENCES_KEY
-        override val defaultValue: String = ""
-        override val preferencesKey: Preferences.Key<String> = stringPreferencesKey(key)
+        override val defaultValue: Boolean = false
+        override val preferencesKey: Preferences.Key<Boolean> = booleanPreferencesKey(key)
     }
 
     data object UserPreferences : ConstantsPreferences<String>() {
