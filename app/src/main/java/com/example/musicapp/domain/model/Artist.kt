d2268938f13304b08a7f2983b782d70ea9d1e971
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-24 16:43:27 -0600

    Config Music App Notification helper part. 52.

--- app/src/main/java/com/example/musicapp/domain/model/Artist.kt
+++ app/src/main/java/com/example/musicapp/domain/model/Artist.kt
@@ -3,8 +3,6 @@ package com.example.musicapp.domain.model
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
-import kotlinx.serialization.Serializable
-
 @Parcelize
 data class Artist(
     val bio: String = "",
