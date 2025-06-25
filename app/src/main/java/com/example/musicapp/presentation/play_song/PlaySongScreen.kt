303fb9ad5145f047ff5b107db49d4ccb0fdb5c32
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-25 13:24:50 -0600

    Play song presentation module part. 8.

--- app/src/main/java/com/example/musicapp/presentation/play_song/PlaySongScreen.kt
+++ app/src/main/java/com/example/musicapp/presentation/play_song/PlaySongScreen.kt
@@ -1,11 +1,29 @@
 package com.example.musicapp.presentation.play_song
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
-import org.koin.android.annotation.KoinViewModel
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavHostController
+import kotlinx.coroutines.flow.collectLatest
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun PlaySongScreen(
-    viewModel: PlaySongViewModel = KoinViewModel()
+    navController: NavHostController,
+    viewModel: PlaySongViewModel = koinViewModel()
 ) {
 
+    val state by viewModel.state.collectAsState()
+
+    LaunchedEffect(key1 = true) {
+        viewModel.effect.collectLatest { effect ->
+            when (effect) {
+                is PlaySongEffect.ShowErrorMessage -> {
+                    Toast.makeText(navController.context, effect.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 }
