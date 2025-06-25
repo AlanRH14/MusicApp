1e13cebaeae61837f96e177d6d243cc332b160a0
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-25 13:12:54 -0600

    Play song presentation module part. 7.

--- app/src/main/java/com/example/musicapp/presentation/play_song/PlaySongScreen.kt
+++ app/src/main/java/com/example/musicapp/presentation/play_song/PlaySongScreen.kt
@@ -1,2 +1,11 @@
 package com.example.musicapp.presentation.play_song
 
+import androidx.compose.runtime.Composable
+import org.koin.android.annotation.KoinViewModel
+
+@Composable
+fun PlaySongScreen(
+    viewModel: PlaySongViewModel = KoinViewModel()
+) {
+
+}
