94076d20a90de41616a7808e67235681d2d1feb6
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-25 17:11:56 -0600

    Play song presentation module part. 13.

--- app/src/main/java/com/example/musicapp/data/remote/repository/MusicRepositoryImp.kt
+++ app/src/main/java/com/example/musicapp/data/remote/repository/MusicRepositoryImp.kt
@@ -1,6 +1,12 @@
 package com.example.musicapp.data.remote.repository
 
-class MusicRepositoryImp() {
+import com.example.musicapp.common.Resource
+import com.example.musicapp.domain.model.Song
+import com.example.musicapp.domain.repository.MusicRepository
 
+class MusicRepositoryImp(): MusicRepository {
 
+    override suspend fun getSongById(): Resource<Song> {
+        TODO("Not yet implemented")
+    }
 }
