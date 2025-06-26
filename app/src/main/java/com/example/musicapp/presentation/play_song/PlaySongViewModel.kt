13c0d86380c368e33ac2b629390d16420c603060
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-26 11:21:03 -0600

    Play song presentation module part. 25.

--- app/src/main/java/com/example/musicapp/presentation/play_song/PlaySongViewModel.kt
+++ app/src/main/java/com/example/musicapp/presentation/play_song/PlaySongViewModel.kt
@@ -1,12 +1,15 @@
 package com.example.musicapp.presentation.play_song
 
 import androidx.lifecycle.ViewModel
+import com.example.musicapp.domain.repository.MusicRepository
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class PlaySongViewModel() : ViewModel() {
+class PlaySongViewModel(
+    private val repository: MusicRepository
+) : ViewModel() {
 
     private val _state = MutableStateFlow(PlaySongState())
     val state = _state.asStateFlow()
