ab8bfd281fc709384a542235cf86e9ec22b11544
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-26 08:10:31 -0600

    Play song presentation module part. 17.

--- app/src/main/java/com/example/musicapp/data/remote/repository/MusicRepositoryImp.kt
+++ app/src/main/java/com/example/musicapp/data/remote/repository/MusicRepositoryImp.kt
@@ -1,12 +1,15 @@
 package com.example.musicapp.data.remote.repository
 
+import com.example.musicapp.common.ApiMapper
 import com.example.musicapp.common.Resource
+import com.example.musicapp.data.model.SongDto
 import com.example.musicapp.data.remote.api.ApiService
 import com.example.musicapp.domain.model.Song
 import com.example.musicapp.domain.repository.MusicRepository
 
 class MusicRepositoryImp(
     private val apiService: ApiService,
+    private val apiSongMapper: ApiMapper<SongDto, Song>
 ) : MusicRepository {
 
     override suspend fun getSongById(): Resource<Song> {
@@ -16,7 +19,7 @@ class MusicRepositoryImp(
             val response = apiService.getSongByID("")
             if (response.isSuccessful) {
                 response.body()?.let { song ->
-                    Resource.Success(Song())
+                    Resource.Success(apiSongMapper.mapToDomain(apiDto = song))
                 } ?: Resource.Success(Song())
             } else {
                 throw Exception("Get song by ID failed")
