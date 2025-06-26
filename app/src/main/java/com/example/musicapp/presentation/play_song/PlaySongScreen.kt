d848bd3f34f073047caf0ac5933d9f54a707a244
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-26 13:23:02 -0600

    Play song presentation module part. 45.

--- app/src/main/java/com/example/musicapp/presentation/play_song/PlaySongScreen.kt
+++ app/src/main/java/com/example/musicapp/presentation/play_song/PlaySongScreen.kt
@@ -5,7 +5,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
+import com.example.musicapp.R
+import com.example.musicapp.presentation.common.widgets.ErrorScreen
 import com.example.musicapp.presentation.common.widgets.LoadingScreen
 import kotlinx.coroutines.flow.collectLatest
 import org.koin.androidx.compose.koinViewModel
@@ -35,5 +38,11 @@ fun PlaySongScreen(
         LoadingScreen()
     }
 
-    if (state.e)
+    if (!state.error.isNullOrEmpty()) {
+        ErrorScreen(
+            errorMessage = state.error ?: "Not Found",
+            primaryButton = stringResource(R.string.retry),
+            onPrimaryButtonClicked = {},
+        )
+    }
 }
