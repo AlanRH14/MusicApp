dece76c32c2845ee861c6f354fa9f26008975a2c
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-06-27 12:00:06 -0600

    Fix error PlaySong Notification part. 34.

--- app/src/main/java/com/example/musicapp/di/MusicModule.kt
+++ app/src/main/java/com/example/musicapp/di/MusicModule.kt
@@ -1,10 +1,11 @@
 package com.example.musicapp.di
 
 import com.example.musicapp.data.service.helper.MusicAppNotificationHelper
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val musicModule = module {
     single {
-        MusicAppNotificationHelper(get())
+        MusicAppNotificationHelper(androidContext())
     }
 }
