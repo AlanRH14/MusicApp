665d0b76b862abac1ea3bb37e520c1d143b16ef6
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-10 14:08:42 -0600

    Config Play Song Screen part. 36.

--- app/src/main/java/com/example/musicapp/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/example/musicapp/presentation/login/LoginViewModel.kt
@@ -11,7 +11,6 @@ import com.example.musicapp.presentation.login.mvi.LoginEffect
 import com.example.musicapp.presentation.login.mvi.LoginState
 import com.example.musicapp.presentation.login.mvi.LoginUIEvent
 import com.example.musicapp.utils.ValidateFormat.emailFormatValid
-import com.example.musicapp.utils.emailFormatValid
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
