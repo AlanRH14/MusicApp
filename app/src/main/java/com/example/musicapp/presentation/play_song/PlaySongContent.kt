36ec3f041701d1ad1e8836e70bc72a8b4c02d185
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-10 10:53:28 -0600

    Config Play Song Screen part. 14.

--- app/src/main/java/com/example/musicapp/presentation/play_song/PlaySongContent.kt
+++ app/src/main/java/com/example/musicapp/presentation/play_song/PlaySongContent.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.musicapp.ui.theme.MusicAppTheme
@@ -26,7 +27,8 @@ fun PlaySongContent(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
-            .padding(PaddingLarge)
+            .padding(PaddingLarge),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         NowPlayingContent(
             title = title,
--- app/src/main/java/com/example/musicapp/presentation/play_song/SongContent.kt
+++ app/src/main/java/com/example/musicapp/presentation/play_song/SongContent.kt
@@ -2,11 +2,13 @@ package com.example.musicapp.presentation.play_song
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -26,9 +28,11 @@ fun SongContent(
 ) {
     Column(
         modifier = Modifier
+            .fillMaxWidth()
     ) {
         MusicAppImage(
-            modifier = Modifier.size(300.dp),
+            modifier = Modifier.size(300.dp)
+                .align(Alignment.CenterHorizontally),
             pathImage = image,
             imageDefault = 0,
             placeHolder = {},
