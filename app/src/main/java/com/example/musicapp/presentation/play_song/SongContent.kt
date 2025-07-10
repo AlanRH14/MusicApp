e12a3eb619745951a2d9d451233e49043afe6b39
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-10 10:37:05 -0600

    Config Play Song Screen part. 9.

---
+++ app/src/main/java/com/example/musicapp/presentation/play_song/SongContent.kt
@@ -0,0 +1,45 @@
+package com.example.musicapp.presentation.play_song
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.musicapp.R
+import com.example.musicapp.presentation.common.components.MusicAppImage
+import com.example.musicapp.ui.theme.PaddingDefault
+
+@Composable
+fun SongContent(
+    title: String,
+    genre: String,
+    image: String,
+    duration: Long,
+    currentPosition: Long,
+    isPlaying: Boolean = false,
+    isBuffering: Boolean = false
+) {
+    Column(
+        modifier = Modifier
+    ) {
+        MusicAppImage(
+            modifier = Modifier.size(300.dp),
+            pathImage = image,
+            imageDefault = R.drawable.ic_logo,
+            placeHolder = {},
+            contentDescription = "Song Cover Image",
+        )
+
+        Spacer(modifier = Modifier.padding(PaddingDefault))
+
+        Text(
+            text = title,
+            style = MaterialTheme.typography.headlineSmall,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
