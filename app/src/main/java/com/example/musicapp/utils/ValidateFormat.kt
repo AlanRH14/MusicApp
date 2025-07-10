0c8ac79585e593d93b22ccc0d05a54480b570619
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-10 14:07:08 -0600

    Config Play Song Screen part. 32.

--- app/src/main/java/com/example/musicapp/utils/ValidateFormat.kt
+++ app/src/main/java/com/example/musicapp/utils/ValidateFormat.kt
@@ -4,14 +4,16 @@ import android.util.Patterns.EMAIL_ADDRESS
 import kotlin.math.pow
 import kotlin.math.round
 
-fun String?.emailFormatValid(): Boolean =
-    EMAIL_ADDRESS.matcher(this ?: "").matches()
+object ValidateFormat {
+    fun String?.emailFormatValid(): Boolean =
+        EMAIL_ADDRESS.matcher(this ?: "").matches()
 
-fun String.passwordFormatValid(): Boolean =
-    true
+    fun String.passwordFormatValid(): Boolean =
+        true
 
-fun Long?.toDecimalValue(decimalPlaces: Int = 2): String {
-    val factor = 10.0.pow(decimalPlaces.toDouble())
-    return (round((this?.toDouble() ?: 0.0) * factor) / factor)
-        .toString()
+    fun Long?.toDecimalValue(decimalPlaces: Int = 2): String {
+        val factor = 10.0.pow(decimalPlaces.toDouble())
+        return (round((this?.toDouble() ?: 0.0) * factor) / factor)
+            .toString()
+    }
 }
