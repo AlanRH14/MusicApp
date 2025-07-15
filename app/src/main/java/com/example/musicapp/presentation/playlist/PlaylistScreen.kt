cd44ceed19b9160f70569e563640b1d9367a6d86
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-15 17:10:13 -0600

    Config Playlist module part. 10.

--- app/src/main/java/com/example/musicapp/presentation/playlist/PlaylistScreen.kt
+++ app/src/main/java/com/example/musicapp/presentation/playlist/PlaylistScreen.kt
@@ -1,8 +1,17 @@
 package com.example.musicapp.presentation.playlist
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavHostController
 
 @Composable
-fun PlaylistScreen() {
+fun PlaylistScreen(
+    navController: NavHostController,
+    viewModel: PlaylistViewModel,
+) {
+
+    val state by viewModel.state.collectAsState()
+
 
 }
