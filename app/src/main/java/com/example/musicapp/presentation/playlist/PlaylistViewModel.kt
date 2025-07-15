b25517cef53181aa6433dbe81546aa8454d352b5
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-15 17:06:28 -0600

    Config Playlist module part. 9.

--- app/src/main/java/com/example/musicapp/presentation/playlist/PlaylistViewModel.kt
+++ app/src/main/java/com/example/musicapp/presentation/playlist/PlaylistViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.musicapp.presentation.playlist
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class PlaylistViewModel : ViewModel() {
 
@@ -16,5 +18,11 @@ class PlaylistViewModel : ViewModel() {
 
     fun onEvent(event: PlaylistUIEvent) {
 
+    }
+
+    private fun showError() {
+        viewModelScope.launch {
+            _effect.emit(PlaylistEffect.ShowMessageError("Error"))
+        }
     }
 }
