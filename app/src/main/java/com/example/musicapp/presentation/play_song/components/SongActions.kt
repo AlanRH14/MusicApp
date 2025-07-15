8ff11bcde88988a4855c81a5b5c58656e58d8c6e
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-15 13:39:37 -0600

    Config Play Song Screen part. 74.

--- app/src/main/java/com/example/musicapp/presentation/play_song/components/SongActions.kt
+++ app/src/main/java/com/example/musicapp/presentation/play_song/components/SongActions.kt
@@ -1,16 +1,23 @@
 package com.example.musicapp.presentation.play_song.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.musicapp.R
+import com.example.musicapp.ui.theme.PaddingDefault
 
 @Composable
 fun SongActions(
@@ -28,7 +35,14 @@ fun SongActions(
             )
         }
 
-        IconButton(onClick = {}) {
+        IconButton(
+            modifier = Modifier
+                .padding(PaddingDefault)
+                .size(80.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.primary),
+            onClick = {}
+        ) {
             Icon(
                 painter = painterResource(
                     id = if (isPlaying) {
