9c374cb275bac77accc3e098b8d4e0204fcbe969
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-15 17:02:50 -0600

    Config Playlist module part. 7.

--- app/src/main/java/com/example/musicapp/presentation/playlist/PlaylistViewModel.kt
+++ app/src/main/java/com/example/musicapp/presentation/playlist/PlaylistViewModel.kt
@@ -1,11 +1,18 @@
 package com.example.musicapp.presentation.playlist
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class PlaylistViewModel : ViewModel() {
 
     private val _state = MutableStateFlow(PlaylistState())
     val state = _state.asStateFlow()
+
+    private val _effect = MutableSharedFlow<PlaylistEffect>()
+    val effect = _effect.asSharedFlow()
+
+
 }
