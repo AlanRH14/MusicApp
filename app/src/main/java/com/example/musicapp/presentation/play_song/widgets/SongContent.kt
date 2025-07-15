00c9b038b87759810cb8123124d438093b659c39
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-15 13:43:06 -0600

    Config Play Song Screen part. 77.

--- app/src/main/java/com/example/musicapp/presentation/play_song/widgets/SongContent.kt
+++ app/src/main/java/com/example/musicapp/presentation/play_song/widgets/SongContent.kt
@@ -1,15 +1,11 @@
 package com.example.musicapp.presentation.play_song.widgets
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
