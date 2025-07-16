2878c8abc648337864c6daffadc56b1442373734
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-16 14:01:55 -0600

    Config PlaylistViewModel part. 5.

--- app/src/main/java/com/example/musicapp/presentation/playlist/PlaylistViewModel.kt
+++ app/src/main/java/com/example/musicapp/presentation/playlist/PlaylistViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.musicapp.presentation.playlist.mvi.PlaylistEffect
 import com.example.musicapp.presentation.playlist.mvi.PlaylistState
+import com.example.musicapp.presentation.playlist.mvi.PlaylistUIEvent
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
