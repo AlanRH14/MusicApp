aa3af7b626b6942d73ffc78b757ef3f75d0db6c1
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-16 11:29:36 -0600

    Config PlayListRepositoryImpl part. 14.

--- app/src/main/java/com/example/musicapp/data/model/PlaylistModelDto.kt
+++ app/src/main/java/com/example/musicapp/data/model/PlaylistModelDto.kt
@@ -1,5 +1,12 @@
 package com.example.musicapp.data.model
 
 data class PlaylistModelDto(
-    val test: String? = null
+    val id: String? = null,
+    val name: String? = null,
+    val description: String? = null,
+    val coverImage: String? = null,
+    val userId: String? = null,
+    val songs: List<SongDto>? = null,
+    val createdAt: Long? = null,
+    val updateAt: Long? = null
 )
