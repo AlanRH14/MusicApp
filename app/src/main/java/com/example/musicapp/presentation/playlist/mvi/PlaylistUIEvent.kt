d85f4d366a12a8430460f361e3d5d520d99241f5
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-16 14:11:20 -0600

    Config PlaylistViewModel part. 9.

--- app/src/main/java/com/example/musicapp/presentation/playlist/mvi/PlaylistUIEvent.kt
+++ app/src/main/java/com/example/musicapp/presentation/playlist/mvi/PlaylistUIEvent.kt
@@ -1,5 +1,6 @@
 package com.example.musicapp.presentation.playlist.mvi
 
 interface PlaylistUIEvent {
+    data object GetPlaylist: PlaylistUIEvent
     data object OnClickedRetry : PlaylistUIEvent
 }
