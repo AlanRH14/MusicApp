0ccb09b107d83e34704eed25f86a64e6c71005ab
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-16 14:28:29 -0600

    Home Access create add playlist

--- app/src/main/java/com/example/musicapp/presentation/home/mvi/HomeEffect.kt
+++ app/src/main/java/com/example/musicapp/presentation/home/mvi/HomeEffect.kt
@@ -1,7 +1,7 @@
 package com.example.musicapp.presentation.home.mvi
 
-sealed class HomeEffect {
-    data class ShowErrorMessage(val message: String) : HomeEffect()
-    data class NavigateToSong(val songID: String): HomeEffect()
-    data object NavigationToPlaylist: HomeEffect()
+sealed interface HomeEffect {
+    data class ShowErrorMessage(val message: String) : HomeEffect
+    data class NavigateToSong(val songID: String): HomeEffect
+    data object NavigationToPlaylist: HomeEffect
 }
