9bd7f5ddae29a3d321676cf754d4ad76194f2d90
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-16 10:57:03 -0600

    Config PlayListRepositoryImpl part. 9.

---
+++ app/src/main/java/com/example/musicapp/data/model/PlaylistModelDto.kt
@@ -0,0 +1,5 @@
+package com.example.musicapp.data.model
+
+data class PlaylistModelDto(
+    val test: String? = null
+)
--- app/src/main/java/com/example/musicapp/domain/repository/PlaylistRepository.kt
+++ app/src/main/java/com/example/musicapp/domain/repository/PlaylistRepository.kt
@@ -1,5 +1,7 @@
 package com.example.musicapp.domain.repository
 
+import com.example.musicapp.data.model.PlaylistModelDto
+
 interface PlaylistRepository {
     suspend fun getPlaylist(): List<PlaylistModelDto>
 }
