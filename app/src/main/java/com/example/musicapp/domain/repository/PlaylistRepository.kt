49ccccb1884af75520a72639dc46e20b92ae65f5
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-16 13:50:20 -0600

    Config PlayListRepositoryImpl part. 32.

--- app/src/main/java/com/example/musicapp/domain/repository/PlaylistRepository.kt
+++ app/src/main/java/com/example/musicapp/domain/repository/PlaylistRepository.kt
@@ -1,8 +1,11 @@
 package com.example.musicapp.domain.repository
 
 import com.example.musicapp.common.Resource
+import com.example.musicapp.data.model.request.CreatePlaylistRequest
 import com.example.musicapp.domain.model.Playlist
 
 interface PlaylistRepository {
     suspend fun getPlaylist(): Resource<List<Playlist>>
+
+    suspend fun createPlaylist(playlistRequest: CreatePlaylistRequest): Resource<List<Playlist>>
 }
