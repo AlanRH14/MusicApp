f184676633c72c33e71bcdfb4ed3b1d0562d2d03
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-16 11:28:25 -0600

    Config PlayListRepositoryImpl part. 14.

--- app/src/main/java/com/example/musicapp/domain/model/Playlist.kt
+++ app/src/main/java/com/example/musicapp/domain/model/Playlist.kt
@@ -1,5 +1,12 @@
 package com.example.musicapp.domain.model
 
 data class Playlist(
-    val test: String? = null
+    val id: String,
+    val name: String,
+    val description: String,
+    val coverImage: String,
+    val userId: String,
+    val songs: List<Song>,
+    val createdAt: Long,
+    val updateAt: Long
 )
