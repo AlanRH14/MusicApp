71be5c2e53ce6de36899dca07cf1511adaca5911
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-16 10:53:47 -0600

    Config PlayListRepositoryImpl part. 4.

--- app/src/main/java/com/example/musicapp/domain/repository/MusicRepository.kt
+++ app/src/main/java/com/example/musicapp/domain/repository/MusicRepository.kt
@@ -4,6 +4,5 @@ import com.example.musicapp.common.Resource
 import com.example.musicapp.domain.model.Song
 
 interface MusicRepository {
-
     suspend fun getSongById(id: String): Resource<Song>
 }
