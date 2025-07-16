d6b4fe576a167be64907003626eb4e14ceb6b148
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-16 17:25:46 -0600

    Home Access create add playlist

--- app/src/main/java/com/example/musicapp/data/model/PlaylistDto.kt
+++ app/src/main/java/com/example/musicapp/data/model/PlaylistDto.kt
@@ -1,12 +1,16 @@
 package com.example.musicapp.data.model
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class PlaylistDto(
-    val id: String? = null,
-    val name: String? = null,
-    val description: String? = null,
-    val coverImage: String? = null,
-    val userId: String? = null,
-    val songs: List<SongDto>? = null,
-    val createdAt: Long? = null,
-    val updateAt: Long? = null
+    @SerialName("id") val id: String? = null,
+    @SerialName("name") val name: String? = null,
+    @SerialName("description") val description: String? = null,
+    @SerialName("coverImage") val coverImage: String? = null,
+    @SerialName("userId") val userId: String? = null,
+    @SerialName("songs") val songs: List<SongDto>? = null,
+    @SerialName("createdAt") val createdAt: Long? = null,
+    @SerialName("updateAt") val updateAt: Long? = null
 )
