c391099a399bfe0fe1f6188812748beb4f9b8ca0
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-16 10:57:49 -0600

    Config PlayListRepositoryImpl part. 10.

--- app/src/main/java/com/example/musicapp/data/remote/repository/PlaylistRepositoryImpl.kt
+++ app/src/main/java/com/example/musicapp/data/remote/repository/PlaylistRepositoryImpl.kt
@@ -1,9 +1,14 @@
 package com.example.musicapp.data.remote.repository
 
+import com.example.musicapp.data.model.PlaylistModelDto
 import com.example.musicapp.data.remote.api.ApiService
+import com.example.musicapp.domain.repository.PlaylistRepository
 
 class PlaylistRepositoryImpl(
     private val apiService: ApiService
-) {
+) : PlaylistRepository {
 
+    override suspend fun getPlaylist(): List<PlaylistModelDto> {
+        TODO("Not yet implemented")
+    }
 }
