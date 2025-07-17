55d21e7ced9f1d63a4be2347776d04b865ca5870
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-17 12:31:26 -0600

    Config CreatePlaylist module part. 8.

--- app/src/main/java/com/example/musicapp/presentation/create_playlist/CreatePlaylistScreen.kt
+++ app/src/main/java/com/example/musicapp/presentation/create_playlist/CreatePlaylistScreen.kt
@@ -1,8 +1,13 @@
 package com.example.musicapp.presentation.create_playlist
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun CreatePlaylistScreen() {
+fun CreatePlaylistScreen(
+    navController: NavHostController,
+    viewModel: CreatePlaylistViewModel = koinViewModel()
+) {
 
 }
