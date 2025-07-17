a9e5c2124a1309193b0933accea8d1f1e673c592
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-17 13:06:58 -0600

    Config CreatePlaylist module part. 20.

--- app/src/main/java/com/example/musicapp/presentation/create_playlist/CreatePlaylistState.kt
+++ app/src/main/java/com/example/musicapp/presentation/create_playlist/CreatePlaylistState.kt
@@ -2,5 +2,7 @@ package com.example.musicapp.presentation.create_playlist
 
 data class CreatePlaylistState(
     val isLoading: Boolean = false,
-    val error: String? = null
+    val error: String? = null,
+    val name: String = "",
+    val description: String = ""
 )
