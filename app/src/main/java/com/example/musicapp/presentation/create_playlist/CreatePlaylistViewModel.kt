b5ea491713f584b21c24f8ec38835c0d1a4fbd25
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-17 12:29:35 -0600

    Config CreatePlaylist module part. 7.

--- app/src/main/java/com/example/musicapp/presentation/create_playlist/CreatePlaylistViewModel.kt
+++ app/src/main/java/com/example/musicapp/presentation/create_playlist/CreatePlaylistViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.musicapp.presentation.create_playlist
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-class CreatePlaylistViewModel: ViewModel() {
+class CreatePlaylistViewModel : ViewModel() {
 
+    private val _state = MutableStateFlow(CreatePlaylistState())
+    val state = _state.asStateFlow()
+
+    private val _effect = MutableSharedFlow<CreatePlaylistEffect>()
+    val effect = _effect.asSharedFlow()
+
+    fun onEvent(event: CreatePlaylistUIEvent) {
+
+    }
 }
