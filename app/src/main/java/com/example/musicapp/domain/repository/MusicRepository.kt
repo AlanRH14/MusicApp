aa34fb31ab74fdb0d042d4ac59467647f19f07c3
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-17 11:10:48 -0600

    Code refactor part. 14

--- app/src/main/java/com/example/musicapp/domain/repository/MusicRepository.kt
+++ app/src/main/java/com/example/musicapp/domain/repository/MusicRepository.kt
@@ -2,7 +2,8 @@ package com.example.musicapp.domain.repository
 
 import com.example.musicapp.common.Resource
 import com.example.musicapp.domain.model.Song
+import kotlinx.coroutines.flow.Flow
 
 interface MusicRepository {
-    suspend fun getSongById(id: String): Resource<Song>
+    fun getSongById(id: String): Flow<Resource<Song>>
 }
