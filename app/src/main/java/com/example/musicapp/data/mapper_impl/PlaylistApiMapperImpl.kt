9dbb359e9584965fa30d98ff92ad3fbfce0fcb11
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-18 13:44:30 -0600

    Config CreatePlaylist module part. 65.

---
+++ app/src/main/java/com/example/musicapp/data/mapper_impl/PlaylistApiMapperImpl.kt
@@ -0,0 +1,27 @@
+package com.example.musicapp.data.mapper_impl
+
+import com.example.musicapp.common.ApiMapper
+import com.example.musicapp.data.model.PlaylistDto
+import com.example.musicapp.data.model.SongDto
+import com.example.musicapp.domain.model.Playlist
+import com.example.musicapp.domain.model.Song
+
+class PlaylistApiMapperImpl(
+    private val apiSongMapper: ApiMapper<SongDto, Song>
+): ApiMapper<PlaylistDto, Playlist> {
+
+    override fun mapToDomain(apiDto: PlaylistDto): Playlist {
+        return Playlist(
+            id = apiDto.id ?: "",
+            name = apiDto.name ?: "",
+            description = apiDto.description ?: "",
+            coverImage = apiDto.coverImage ?: "",
+            userId = apiDto.userId ?: "",
+            songs = apiDto.songs?.map { song ->
+                apiSongMapper.mapToDomain(apiDto = song)
+            } ?: emptyList(),
+            createdAt = apiDto.createdAt ?: 0L,
+            updateAt = apiDto.updateAt ?: 0L
+        )
+    }
+}
