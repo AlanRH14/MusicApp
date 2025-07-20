49b259e6620ac5731a873808bb139fc53e161d29
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-07-20 16:30:12 -0600

    Refactor code part. 5.

--- app/src/main/java/com/example/musicapp/utils/Constants.kt
+++ app/src/main/java/com/example/musicapp/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.musicapp.utils
 
 object Constants {
-    const val BASE_URL = "http://192.168.1.114:8080/"
+    const val BASE_URL = "http://10.0.2.2:8080/"
     const val GRID_CELLS_SIZE = 2
     const val AUTHENTICATION_HEADER_TYPE = "Bearer"
 }
