8c45c8951b2f8e0e076678751c34481260b935d3
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-21 12:37:37 -0600

    Clean project part. 2.

--- app/src/main/java/com/example/musicapp/data/local/database/dao/MusicAppDao.kt
+++ app/src/main/java/com/example/musicapp/data/local/database/dao/MusicAppDao.kt
@@ -2,7 +2,6 @@ package com.example.musicapp.data.local.database.dao
 
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
-import androidx.room.Update
 
 interface MusicAppDao<T> {
 
