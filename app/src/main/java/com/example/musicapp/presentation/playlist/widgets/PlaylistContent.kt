eb409ea5e068be8090d2826e195f78ca775b131a
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-21 11:29:29 -0600

    Create playlist item part. 5.

--- app/src/main/java/com/example/musicapp/presentation/playlist/widgets/PlaylistContent.kt
+++ app/src/main/java/com/example/musicapp/presentation/playlist/widgets/PlaylistContent.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.musicapp.presentation.playlist.PlaylistItem
+import com.example.musicapp.presentation.playlist.components.PlaylistItem
 import com.example.musicapp.presentation.playlist.mvi.PlaylistState
 import com.example.musicapp.presentation.playlist.mvi.PlaylistUIEvent
 
