a0799130c897f7d4074ecdbaea6d58f116facbde
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-21 10:35:55 -0600

    Refactor code part. 18.

---
+++ app/src/main/java/com/example/musicapp/presentation/playlist/PlaylistItem.kt
@@ -0,0 +1,30 @@
+package com.example.musicapp.presentation.playlist
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.musicapp.R
+import com.example.musicapp.domain.model.Playlist
+import com.example.musicapp.presentation.common.components.MusicAppImage
+import com.example.musicapp.presentation.playlist.mvi.PlaylistState
+import com.example.musicapp.ui.theme.PaddingDefault
+
+@Composable
+fun PlaylistItem(
+    playlist: Playlist
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(PaddingDefault)
+    ) {
+        MusicAppImage(
+            pathImage = playlist.coverImage,
+            imageDefault = R.drawable.ic_logo,
+            placeHolder = {},
+            contentDescription = "Playlist Item Image"
+        )
+    }
+}
