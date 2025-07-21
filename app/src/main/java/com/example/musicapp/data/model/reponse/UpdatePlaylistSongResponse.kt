1153feeff72a80a28e692de41643446cd423c4d6
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-21 16:57:58 -0600

    Config repository AddSongToPlaylist part. 4.

---
+++ app/src/main/java/com/example/musicapp/data/model/reponse/UpdatePlaylistSongResponse.kt
@@ -0,0 +1,10 @@
+package com.example.musicapp.data.model.reponse
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UpdatePlaylistSongResponse(
+    @SerialName("songsAdded")
+    val songsAdded: List<String>? = null
+)
