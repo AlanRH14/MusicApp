4776cd0b0a6c618ba255a40a5bd8774d60e7a70b
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-21 09:20:26 -0600

    Refactor code part. 7.

--- app/src/main/java/com/example/musicapp/di/DatasourceModule.kt
+++ app/src/main/java/com/example/musicapp/di/DatasourceModule.kt
@@ -4,11 +4,15 @@ import com.example.musicapp.data.local.datasource.UserLocalDataSource
 import com.example.musicapp.data.local.datasource.UserLocalDataSourceImpl
 import com.example.musicapp.data.remote.datasource.RemoteAuthDataSource
 import com.example.musicapp.data.remote.datasource.RemoteAuthDataSourceImpl
+import kotlinx.coroutines.Dispatchers
 import org.koin.dsl.module
 
 val datasourceModule = module {
     single<RemoteAuthDataSource> {
-        RemoteAuthDataSourceImpl(get())
+        RemoteAuthDataSourceImpl(
+            apiService = get(),
+            ioDispatcher = Dispatchers.IO
+        )
     }
 
     single<UserLocalDataSource> {
