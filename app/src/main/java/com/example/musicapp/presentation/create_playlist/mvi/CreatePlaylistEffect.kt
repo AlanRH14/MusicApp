31ed9c8fbdc3a89f53e747678fc79ad316680c07
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-21 09:38:45 -0600

    Refactor code part. 9.

--- app/src/main/java/com/example/musicapp/presentation/create_playlist/mvi/CreatePlaylistEffect.kt
+++ app/src/main/java/com/example/musicapp/presentation/create_playlist/mvi/CreatePlaylistEffect.kt
@@ -1,7 +1,5 @@
 package com.example.musicapp.presentation.create_playlist.mvi
 
-import com.example.musicapp.domain.model.Playlist
-
 sealed interface CreatePlaylistEffect {
     data class ShowErrorMessage(val message: String) : CreatePlaylistEffect
     data object NavigateToPlaylist: CreatePlaylistEffect
