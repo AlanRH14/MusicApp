380d17e8e4d5f8faf185c67e06a18b8d151e8f59
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-22 08:08:57 -0600

    Config repository AddSongToPlaylist part. 14.

--- app/src/main/java/com/example/musicapp/presentation/play_song/PlaySongScreen.kt
+++ app/src/main/java/com/example/musicapp/presentation/play_song/PlaySongScreen.kt
@@ -1,13 +1,18 @@
 package com.example.musicapp.presentation.play_song
 
 import android.widget.Toast
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import com.example.musicapp.R
+import com.example.musicapp.domain.model.Playlist
 import com.example.musicapp.presentation.common.widgets.ErrorScreen
 import com.example.musicapp.presentation.common.widgets.LoadingScreen
 import com.example.musicapp.presentation.play_song.mvi.PlaySongEffect
@@ -16,6 +21,7 @@ import com.example.musicapp.presentation.play_song.widgets.PlaySongContent
 import kotlinx.coroutines.flow.collectLatest
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PlaySongScreen(
     navController: NavHostController,
@@ -23,6 +29,9 @@ fun PlaySongScreen(
     songID: String,
 ) {
     val state by viewModel.state.collectAsState()
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = false)
+    val shouldShowSheet = remember { mutableStateOf(false) }
+    val playlist = remember { mutableStateOf<List<Playlist>>(emptyList()) }
 
     LaunchedEffect(key1 = true) {
         viewModel.onEvent(PlaySongUIEvent.GetSongByID(songID = songID))
@@ -33,9 +42,9 @@ fun PlaySongScreen(
                     Toast.makeText(navController.context, effect.message, Toast.LENGTH_SHORT).show()
                 }
 
-                is PlaySongEffect.NavigateToPlaylist -> {
+                is PlaySongEffect.NavigateToPlaylist -> {}
 
-                }
+                is PlaySongEffect.ShowPlaylistSelection -> {}
             }
         }
     }
