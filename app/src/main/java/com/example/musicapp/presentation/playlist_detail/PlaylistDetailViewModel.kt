c0481329a879fc45209ceadf3aa9ced4583d2615
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-23 11:10:22 -0600

    Add playlist config part. 9.

--- app/src/main/java/com/example/musicapp/presentation/playlist_detail/PlaylistDetailViewModel.kt
+++ app/src/main/java/com/example/musicapp/presentation/playlist_detail/PlaylistDetailViewModel.kt
@@ -2,12 +2,14 @@ package com.example.musicapp.presentation.playlist_detail
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.musicapp.common.Resource
 import com.example.musicapp.domain.repository.PlaylistRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class PlaylistDetailViewModel(
@@ -24,7 +26,26 @@ class PlaylistDetailViewModel(
 
     private fun getPlaylistDetails(playlistID: String) {
         viewModelScope.launch(Dispatchers.IO) {
+            repository.getPlaylistDetails(playlistID = playlistID).collect { details ->
+                when (details) {
+                    is Resource.Loading -> {
+                        _state.update { it.copy(isLoading = true) }
+                    }
 
+                    is Resource.Success -> {
+                        _state.update { it.copy(isLoading = false) }
+                    }
+
+                    is Resource.Error -> {
+                        _state.update {
+                            it.copy(
+                                isLoading = false,
+                                error = details.message
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
