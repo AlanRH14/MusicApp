c2daa141a13ff5c0d9c25811f7237b9f41d71d11
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-23 11:31:58 -0600

    Detail playlist config part. 2.

--- app/src/main/java/com/example/musicapp/presentation/playlist_detail/PlaylistDetailContent.kt
+++ app/src/main/java/com/example/musicapp/presentation/playlist_detail/PlaylistDetailContent.kt
@@ -2,12 +2,18 @@ package com.example.musicapp.presentation.playlist_detail
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,11 +23,13 @@ import androidx.compose.ui.unit.dp
 import com.example.musicapp.R
 import com.example.musicapp.domain.model.Playlist
 import com.example.musicapp.presentation.common.components.MusicAppImage
+import com.example.musicapp.ui.theme.PaddingDefault
 import com.example.musicapp.ui.theme.PaddingLarge
 
 @Composable
 fun PlaylistDetailContent(
     playlist: Playlist,
+    onEvent: (PlaylistDetailUIEvent) -> Unit
 ) {
     LazyColumn {
         item {
@@ -58,6 +66,30 @@ fun PlaylistDetailContent(
                     placeHolder = {},
                     contentDescription = "Song Image"
                 )
+
+                Column {
+                    Text(
+                        modifier = Modifier.padding(start = PaddingDefault),
+                        text = song.title,
+                        style = MaterialTheme.typography.bodyLarge,
+                    )
+                    Text(
+                        modifier = Modifier.padding(start = PaddingDefault),
+                        text = song.artist.name,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+
+                Spacer(modifier = Modifier.weight(1F))
+
+                IconButton(onClick = {
+                    onEvent(PlaylistDetailUIEvent.OnDeleteSongOfPlaylist(songID = song.id))
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Delete Song of Playlist Icon"
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/example/musicapp/presentation/playlist_detail/PlaylistDetailUIEvent.kt
+++ app/src/main/java/com/example/musicapp/presentation/playlist_detail/PlaylistDetailUIEvent.kt
@@ -1,4 +1,5 @@
 package com.example.musicapp.presentation.playlist_detail
 
 sealed interface PlaylistDetailUIEvent {
+    data class OnDeleteSongOfPlaylist(val songID: String) : PlaylistDetailUIEvent
 }
