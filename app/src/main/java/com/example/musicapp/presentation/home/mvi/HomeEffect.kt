dc4026af9b9e34fc4ba7ed7798bd829f5581adae
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-24 16:30:40 -0600

    Refactor part. 36.

--- app/src/main/java/com/example/musicapp/presentation/home/mvi/HomeEffect.kt
+++ app/src/main/java/com/example/musicapp/presentation/home/mvi/HomeEffect.kt
@@ -3,5 +3,5 @@ package com.example.musicapp.presentation.home.mvi
 sealed interface HomeEffect {
     data class ShowMessage(val message: String) : HomeEffect
     data class NavigateToPlaySong(val songID: String): HomeEffect
-    data object NavigationToPlaylist: HomeEffect
+    data object NavigateToPlaylist: HomeEffect
 }
