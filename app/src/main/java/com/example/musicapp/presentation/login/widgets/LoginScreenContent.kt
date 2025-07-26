425abca0a5d370bfcb4efcebc24228fb1b1700cc
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2025-07-25 23:09:54 -0600

    Refactor code part. 22.

--- app/src/main/java/com/example/musicapp/presentation/login/widgets/LoginScreenContent.kt
+++ app/src/main/java/com/example/musicapp/presentation/login/widgets/LoginScreenContent.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
