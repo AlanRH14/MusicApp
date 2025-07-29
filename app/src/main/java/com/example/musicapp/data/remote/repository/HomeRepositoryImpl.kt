a9284a303bb9175c0dc49ee84f5e37591f48e687
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-29 16:27:39 -0600

    Update libs versions part. 10.

--- app/src/main/java/com/example/musicapp/data/remote/repository/HomeRepositoryImpl.kt
+++ app/src/main/java/com/example/musicapp/data/remote/repository/HomeRepositoryImpl.kt
@@ -9,6 +9,7 @@ import com.example.musicapp.common.Resource
 import com.example.musicapp.data.local.datasource.UserLocalDataSource
 import com.example.musicapp.utils.Constants.AUTHENTICATION_HEADER_TYPE
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 
 class HomeRepositoryImpl(
@@ -19,19 +20,19 @@ class HomeRepositoryImpl(
 
     override fun getHomeData(): Flow<Resource<Home>> = flow {
         emit(Resource.Loading)
-        try {
-            userLocalDataSource.getUser()?.let { userData ->
-                val response = apiService.getHome(token = "$AUTHENTICATION_HEADER_TYPE ${userData.token}")
-                if (response.isSuccessful) {
-                    response.body()?.let { res ->
-                        emit(Resource.Success(data = apiHomeMapper.mapToDomain(apiDto = res)))
-                    } ?: Resource.Success(data = Home())
-                } else {
-                    throw Exception(response.message())
-                }
+
+        userLocalDataSource.getUser()?.let { userData ->
+            val response =
+                apiService.getHome(token = "$AUTHENTICATION_HEADER_TYPE ${userData.token}")
+            if (response.isSuccessful) {
+                response.body()?.let { res ->
+                    emit(Resource.Success(data = apiHomeMapper.mapToDomain(apiDto = res)))
+                } ?: Resource.Success(data = Home())
+            } else {
+                throw Exception(response.message())
             }
-        } catch (e: Exception) {
-            emit(Resource.Error("Error: ${e.message}"))
         }
+    }.catch { error ->
+        emit(Resource.Error("Error: ${error.message}"))
     }
 }
