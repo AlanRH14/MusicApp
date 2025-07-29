faac03c9fe2621b082eb8aee1a73612bbf0cc7be
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-07-29 16:40:14 -0600

    Clean code part. 3.

--- app/src/main/java/com/example/musicapp/data/remote/repository/PlaylistRepositoryImpl.kt
+++ app/src/main/java/com/example/musicapp/data/remote/repository/PlaylistRepositoryImpl.kt
@@ -24,23 +24,21 @@ class PlaylistRepositoryImpl(
     private val ioDispatcher: CoroutineDispatcher
 ) : PlaylistRepository {
 
-    override fun getPlaylist(): Flow<Resource<List<Playlist>>> = flow {
+    override fun getPlaylist(): Flow<Resource<List<Playlist>>> = flow<Resource<List<Playlist>>> {
         emit(Resource.Loading)
-        try {
-            userLocalDataSource.getUser()?.let { userData ->
-                val response =
-                    apiService.getPlaylist(token = "$AUTHENTICATION_HEADER_TYPE ${userData.token}")
-                if (response.isSuccessful) {
-                    response.body()?.let { res ->
-                        emit(Resource.Success(data = res.map { apiPlaylistMapper.mapToDomain(apiDto = it) }))
-                    } ?: emit(Resource.Success(data = emptyList()))
-                } else {
-                    throw Exception(response.message())
-                }
-            } ?: throw Exception("Get local user error")
-        } catch (e: Exception) {
-            emit(Resource.Error(message = "Error: ${e.message}"))
-        }
+        userLocalDataSource.getUser()?.let { userData ->
+            val response =
+                apiService.getPlaylist(token = "$AUTHENTICATION_HEADER_TYPE ${userData.token}")
+            if (response.isSuccessful) {
+                response.body()?.let { res ->
+                    emit(Resource.Success(data = res.map { apiPlaylistMapper.mapToDomain(apiDto = it) }))
+                } ?: emit(Resource.Success(data = emptyList()))
+            } else {
+                throw Exception(response.message())
+            }
+        } ?: throw Exception("Get local user error")
+    }.catch { error ->
+        emit(Resource.Error(message = "Error: ${error.message}"))
     }
 
     override suspend fun createPlaylist(playlistRequest: CreatePlaylistRequest): Resource<Playlist> =
