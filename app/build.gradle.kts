bc9ccab0ae9a2e61babfaefa1f8225eac2a9a8aa
Alan Andrés Rosas Hernández <alanarh.14@gmail.com>
2026-08-08 17:47:27 -0600

    Fix errors update agp version part. 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.musicapp"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.musicapp"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
